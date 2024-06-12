0bdaac923eb5d5351970e2d07a623d7074f27d28
Tetiana Bortnyk <tatyanabortnik@gmail.com>
2024-06-13 00:46:22 +0200

    fixed google translate widget

--- client/src/utils/googleTranslate.js
+++ client/src/utils/googleTranslate.js
@@ -1,32 +1,3 @@
-// export function loadGoogleTranslateScript(callback) {
-//   if (document.getElementById('google-translate-script')) {
-//     callback();
-//     return;
-//   }
+//TODO add possibility to restore original language:
 
-//   const script = document.createElement('script');
-//   script.id = 'google-translate-script';
-//   script.type = 'text/javascript';
-//   script.src =
-//     '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
-//   script.async = true;
-//   script.defer = true;
-//   script.onload = callback;
-//   document.body.appendChild(script);
-// }
-
-// export function initializeGoogleTranslate() {
-//   if (
-//     window.google &&
-//     window.google.translate &&
-//     window.google.translate.TranslateElement
-//   ) {
-//     new window.google.translate.TranslateElement(
-//       {
-//         pageLanguage: 'en',
-//         layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
-//       },
-//       'google_translate_element'
-//     );
-//   }
-// }
+//https://stackoverflow.com/questions/16281414/google-translator-how-to-manually-restore-original-language-with-javascript
--- server/controller.js
+++ server/controller.js
@@ -1,12 +1,7 @@
 const Business = require('./models/business');
 const Category = require('./models/categories');
 
-const deepl = require('deepl-node');
-const fs = require('fs');
-const path = require('path');
-
 exports.getBusinesses = async (_req, res) => {
-  // console.log(process.env);
   try {
     const businesses = await Business.find();
     res.status(200).json(businesses);
@@ -24,16 +19,16 @@ exports.getCategories = async (_req, res) => {
   }
 };
 
-exports.getBusinessById = async (req, res) => {
-  try {
-    const business = await Business.findById(req.params.id);
-    if (!business) {
-      return res.status(404).json({ message: 'Business not found' });
-    }
-    res.status(200).json(business);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+//-- I dont use it, but I should have ---//
 
-exports.translate = async (req, res) => {};
+// exports.getBusinessById = async (req, res) => {
+//   try {
+//     const business = await Business.findById(req.params.id);
+//     if (!business) {
+//       return res.status(404).json({ message: 'Business not found' });
+//     }
+//     res.status(200).json(business);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };

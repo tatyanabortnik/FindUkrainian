ee278b4c29c350dacc1e1c7c83fefbfd371f0d68
Badreddine El Rhoul <badrhoul@gmail.com>
2024-06-14 13:33:48 +0200

    quick comment

--- server/models/categories.js
+++ server/models/categories.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
   //TODO: remove _id -> adjust related functionality
-  //resolution: Schema will remain unchanged.
+  //Resolution: Schema will remain unchanged.
   _id: {
     type: String,
     required: true,

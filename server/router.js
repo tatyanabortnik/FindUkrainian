7a2ada5d1ed05c2a7b34eb4ce4ae69139f4e5f8c
Regina Czech <regina.czech@yahoo.com>
2024-06-14 13:14:11 +0200

    fix: get By ID routes

--- server/router.js
+++ server/router.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const controller = require('./controller');
 
-router.get('/businesses', controller.getBusinesses);
-router.get('/categories', controller.getCategories);
-//TODO: add getBusinessById get function
+router.get('/businesses', controller.getAllBusinesses);
+router.get('/categories', controller.getAllCategories);
+router.get("/businesses/:id", controller.getBusinessById);
+router.get("categories/:id", controller.getCategoryById);
 
 module.exports = router;

6b155a6a68cd0d7ce03ff7857ee3ff9b3a9ddcc5
Regina Czech <regina.czech@yahoo.com>
2024-06-14 16:13:42 +0200

    fix: getByIdMiddleware refactor

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,5 @@
 const Business = require("./models/business");
-const Category = require("./models/categories");
+const Category = require("./models/category");
 
 exports.getAllMiddleware = (modelName) => {
   return async (_req, res) => {
@@ -12,26 +12,16 @@ exports.getAllMiddleware = (modelName) => {
   };
 };
 
-exports.getBusinessById = async (req, res) => {
-  try {
-    const business = await Business.findById(req.params.id);
-    if (!business) {
-      return res.status(404).json({ message: "Business not found" });
+exports.getByIdMiddleware = (modelName) => {
+  return async (req, res) => {
+    try {
+      const result = await modelName.findById(req.params.id);
+      if (!result) {
+        return res.status(404).json({ message: "Result not found" });
+      }
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-    res.status(200).json(business);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-exports.getCategoryById = async (req, res) => {
-  try {
-    const category = await Category.findById(req.params.id);
-    if (!category) {
-      return res.status(404).json({ message: "Category not found" });
-    }
-    res.status(200).json(category);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
   }
 };
--- server/router.js
+++ server/router.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const controller = require('./controller');
 const Business = require("./models/business");
-const Category = require("./models/categories");
+const Category = require("./models/category");
 
 router.get("/businesses", controller.getAllMiddleware(Business));
 router.get("/categories", controller.getAllMiddleware(Category));
-router.get("/businesses/:id", controller.getBusinessById);
-router.get("categories/:id", controller.getCategoryById);
+router.get("/businesses/:id", controller.getByIdMiddleware(Business));
+router.get("/categories/:id", controller.getByIdMiddleware(Category));
 
 module.exports = router;

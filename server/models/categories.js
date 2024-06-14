f988b0f1c64f25e101df9b17d1f9bc02d0d8ab0f
Badreddine El Rhoul <badrhoul@gmail.com>
2024-06-14 13:04:16 +0200

    Decided seed data will remain untouched.

--- server/models/categories.js
+++ server/models/categories.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 //TODO: review the 'require' for the UK hardcoded translation
+//Resolution: seed data requires this schema to remain unchanged.
 
 const categorySchema = new mongoose.Schema({
   //TODO: remove _id -> adjust related functionality
+  //resolution: Schema will remain unchanged.
   _id: {
     type: String,
     required: true,

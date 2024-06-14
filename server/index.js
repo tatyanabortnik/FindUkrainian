712eeb8c34854514f86b68972be85656968526dc
Regina Czech <regina.czech@yahoo.com>
2024-06-14 16:19:11 +0200

    fix: updated config data in index.js

--- server/index.js
+++ server/index.js
@@ -1,20 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const router = require('./router.js');
-const { connectDB } = require('./models');
+const express = require("express");
+const cors = require("cors");
+const router = require("./router.js");
+const { PORT } = require("./config.js");
+const { connectDB } = require("./models/index.js");
 
-// require('dotenv').config();
-// const { PORT } = require("/")
+require("dotenv").config();
 
 const app = express();
-const PORT = 3000;  //TODO: move to config - make dynamic with .env
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
 app.use((_req, res) => {
-  res.status(404).send('This route does not exist');
+  res.status(404).send("This route does not exist");
 });
 
 (async () => {

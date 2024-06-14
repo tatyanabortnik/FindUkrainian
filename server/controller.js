3bc87140bb10c56e02b8c01871061d00d84b0899
Regina Czech <regina.czech@yahoo.com>
2024-06-14 15:55:22 +0200

    fix: refactor to getAllMiddleware

--- server/controller.js
+++ server/controller.js
@@ -1,31 +1,22 @@
-const Business = require('./models/business');
-const Category = require('./models/categories');
+const Business = require("./models/business");
+const Category = require("./models/categories");
 
-//TODO: refactor into a single function passing the model name as a parameter
-
-exports.getAllBusinesses = async (_req, res) => {
-  try {
-    const businesses = await Business.find();
-    res.status(200).json(businesses);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-exports.getAllCategories = async (_req, res) => {
-  try {
-    const categories = await Category.find();
-    res.status(200).json(categories);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+exports.getAllMiddleware = (modelName) => {
+  return async (_req, res) => {
+    try {
+      const result = await modelName.find();
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
 };
 
 exports.getBusinessById = async (req, res) => {
   try {
     const business = await Business.findById(req.params.id);
     if (!business) {
-      return res.status(404).json({ message: 'Business not found' });
+      return res.status(404).json({ message: "Business not found" });
     }
     res.status(200).json(business);
   } catch (error) {

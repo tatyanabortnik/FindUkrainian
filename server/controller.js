1c682b38984ba974c761c276a905e10253cc2cb6
Regina Czech <regina.czech@yahoo.com>
2024-06-14 12:52:19 +0200

    fix: getBusinessesByID function

--- server/controller.js
+++ server/controller.js
@@ -2,7 +2,7 @@ const Business = require('./models/business');
 const Category = require('./models/categories');
 
 //TODO: refactor into a single function passing the model name as a parameter
-exports.getBusinesses = async (_req, res) => {
+exports.getAllBusinesses = async (_req, res) => {
   try {
     const businesses = await Business.find();
     res.status(200).json(businesses);
@@ -11,7 +11,7 @@ exports.getBusinesses = async (_req, res) => {
   }
 };
 
-exports.getCategories = async (_req, res) => {
+exports.getAllCategories = async (_req, res) => {
   try {
     const categories = await Category.find();
     res.status(200).json(categories);
@@ -20,17 +20,16 @@ exports.getCategories = async (_req, res) => {
   }
 };
 
-//TODO: reimplement this function and use the params
-//-- I dont use it, but I should have ---//
+exports.getBusinessById = async (req, res) => {
+  try {
+    const business = await Business.findById(req.params.id);
+    if (!business) {
+      return res.status(404).json({ message: 'Business not found' });
+    }
+    res.status(200).json(business);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-// exports.getBusinessById = async (req, res) => {
-//   try {
-//     const business = await Business.findById(req.params.id);
-//     if (!business) {
-//       return res.status(404).json({ message: 'Business not found' });
-//     }
-//     res.status(200).json(business);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
+//TODO: get categories by id

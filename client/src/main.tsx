84d3a2691ecb9dbaebfaec9250ebac118f26f99e
Regina Czech <regina.czech@yahoo.com>
2024-06-15 17:15:15 +0200

    fix: main ts createRoot error fixed

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>

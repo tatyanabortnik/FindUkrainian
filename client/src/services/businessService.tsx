32205574fcd8162dbb178a3c78b51a5de2f6207a
Harriet <Harriet.mae.morris@gmail.com>
2024-06-15 12:17:47 +0200

    promise types declared

--- client/src/services/businessService.tsx
+++ client/src/services/businessService.tsx
@@ -1,18 +1,22 @@
 // import {baseUrl} from '../config.js'; //TODO: move to a config file / process.env
+
+import { BusinessType } from "../Types/BusinessType";
+import { CategoryType } from "../Types/CategoryType";
+
 // import { baseUrl } from "../../config";
 const baseUrl: string = 'http://localhost:3000'
 
-export const getBusinesses = (): Promise<any> => getAtEndpoint('businesses');
-export const getCategories = (): Promise<any> => getAtEndpoint('categories')
+export const getBusinesses = (): Promise<BusinessType[]> => getAtEndpoint('businesses');
+export const getCategories = (): Promise<CategoryType[]> => getAtEndpoint('categories')
 
 const getAtEndpoint = async (endpoint: string): Promise<any> => {
-
   try {
     const res = await fetch(`${baseUrl}/${endpoint}`);
     const result = await res.json();
     return result;
   } catch (error) {
     console.error(`Failed to fetch ${endpoint}: ${error}`);
+    return [];
   }
 }
 
@@ -28,4 +32,8 @@ export const getBusinessesByID = async (ID: string): Promise<any> => {
   };
 };
 
+
+function async<T>(endpoint: any, string: any) {
+  throw new Error("Function not implemented.");
+}
 

0b7bdb40c66a2f4f46b899d28fd9e085f4c28514
Harriet <Harriet.mae.morris@gmail.com>
2024-06-15 16:01:38 +0200

    utils converted to ts

--- client/src/utils/isOpen.ts
+++ client/src/utils/isOpen.ts
@@ -1,11 +1,15 @@
-export default function isOpen(openingHours) {
+interface OpenCloseHours {
+  open: number | null;
+  close: number | null;
+}
+
+export default function isOpen(openingHours: {[key: string]: OpenCloseHours}) {
   const now = new Date();
   const dayOfWeek = now.toLocaleString('en-us', { weekday: 'long' });
   const currentMinutes = now.getHours() * 60 + now.getMinutes();
   const hours = openingHours[dayOfWeek];
-  if (!hours || hours.open === null) {
+  if (!hours || hours.open === null || hours.close === null) {
     return false;
   }
-
   return currentMinutes >= hours.open && currentMinutes < hours.close;
 }

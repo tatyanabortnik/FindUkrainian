372b8010fd7ea19cfc326746228b3e6d9d3cac4d
Badreddine El Rhoul <badrhoul@gmail.com>
2024-06-15 16:36:32 +0200

    Theme nad searchList refactored to Ts

--- client/src/components/SearchList/SearchList.tsx
+++ client/src/components/SearchList/SearchList.tsx
@@ -2,9 +2,16 @@
 import { List } from '@mui/material';
 import './style.css';
 import BusinessHandleButton from '../BusinessHandleButton/BusinessHandleButton';
-import { useTheme } from '@emotion/react';
+// import { useTheme } from '@emotion/react';
+import {useTheme} from '@mui/material'
+import { BusinessType } from '../../Types/BusinessType';
 
-export default function SearchList({ foundBusinesses, handleBusinessClick }) {
+interface businessProp {
+  foundBusinesses: BusinessType[];
+  handleBusinessClick: () => void;
+}
+
+export default function SearchList({ foundBusinesses, handleBusinessClick }: businessProp) {
   const { palette } = useTheme();
 
   return (

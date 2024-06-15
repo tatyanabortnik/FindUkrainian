25082c3f1d566861ccf361529627ab87a0c7d767
Badreddine El Rhoul <badrhoul@gmail.com>
2024-06-15 12:27:13 +0200

    moved all image files to public folder. typescript translation complete

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import logo from '../../assets/logo.png';
+
 import './style.css';
 import Avatar from '@mui/material/Avatar';
 import { IconButton, Badge, Box } from '@mui/material';
@@ -6,22 +6,21 @@ import { useTheme } from '@mui/material/styles';
 import Search from '../Search/Search';
 import { Link } from 'react-router-dom';
 
+
 export default function Header() {
   const { palette } = useTheme();
 
   return (
-    //TODO: review MUI sx placements for all below
     <Box className="header" sx={{ borderColor: palette.primary.main }}>
       <div className="header__logo">
-        {/* TODO: remove href */}
-        <Link to={'/'} href="#" className="logo">
-          <img src={logo} alt="logo" />
+
+        <Link to={'/'} className="logo">
+          <img src={'/logo.png'} alt="logo" />
         </Link>
       </div>
       <div className="header__right">
         <Search />
 
-        {/* FIXME: Review google translate button */}
         {/* this works funny and sometimes doesnt load. To see return to original btn, comment out 'iframe' rule in style.css */}
         <div id="google_translate_element" className="language"></div>
 

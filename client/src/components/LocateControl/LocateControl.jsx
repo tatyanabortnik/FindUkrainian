0f56da8f36cdd6835ff6f727c62fd66bfb9ed4b2
Tetiana Bortnyk <tatyanabortnik@gmail.com>
2024-07-06 10:24:44 +0200

    refactor: remove comments and files

--- client/src/components/LocateControl/LocateControl.jsx
+++ client/src/components/LocateControl/LocateControl.jsx
@@ -1,6 +1,6 @@
-import 'leaflet.locatecontrol' // Import plugin
-import 'leaflet.locatecontrol/dist/L.Control.Locate.min.css' // Import styles
-import L from 'leaflet' // Import L from leaflet to start using the plugin
+import 'leaflet.locatecontrol'
+import 'leaflet.locatecontrol/dist/L.Control.Locate.min.css'
+import L from 'leaflet'
 
 import { useEffect } from 'react'
 import { useMap } from 'react-leaflet'
--- server/controller.js
+++ server/controller.js
@@ -1,34 +1,20 @@
-const Business = require('./models/business');
-const Category = require('./models/categories');
+const Business = require('./models/business')
+const Category = require('./models/categories')
 
 exports.getBusinesses = async (_req, res) => {
   try {
-    const businesses = await Business.find();
-    res.status(200).json(businesses);
+    const businesses = await Business.find()
+    res.status(200).json(businesses)
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
 
 exports.getCategories = async (_req, res) => {
   try {
-    const categories = await Category.find();
-    res.status(200).json(categories);
+    const categories = await Category.find()
+    res.status(200).json(categories)
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
-
-//-- I dont use it, but I should have ---//
-
-// exports.getBusinessById = async (req, res) => {
-//   try {
-//     const business = await Business.findById(req.params.id);
-//     if (!business) {
-//       return res.status(404).json({ message: 'Business not found' });
-//     }
-//     res.status(200).json(business);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
+}

9edf54b3c0eb18e7829e003b11bccd1275e74bdb
Tetiana Bortnyk <tatyanabortnik@gmail.com>
2024-07-06 10:20:31 +0200

    fix: remove logs

--- client/src/components/LocateControl/LocateControl.jsx
+++ client/src/components/LocateControl/LocateControl.jsx
@@ -1,26 +1,22 @@
-import 'leaflet.locatecontrol'; // Import plugin
-import 'leaflet.locatecontrol/dist/L.Control.Locate.min.css'; // Import styles
-import L from 'leaflet'; // Import L from leaflet to start using the plugin
+import 'leaflet.locatecontrol' // Import plugin
+import 'leaflet.locatecontrol/dist/L.Control.Locate.min.css' // Import styles
+import L from 'leaflet' // Import L from leaflet to start using the plugin
 
-import { useEffect } from 'react';
-import { useMap } from 'react-leaflet';
+import { useEffect } from 'react'
+import { useMap } from 'react-leaflet'
 
 const LocateControl = () => {
-  const map = useMap();
-
-  // console.log('LocateControl component is rendering');
+  const map = useMap()
 
   useEffect(() => {
-    // console.log('LocateControl component mounted');
-    const location = L.control.locate().addTo(map);
+    const location = L.control.locate().addTo(map)
 
     return () => {
-      map.removeControl(location);
-      // console.log('LocateControl component UNmounted');
-    };
-  }, [map]);
+      map.removeControl(location)
+    }
+  }, [map])
 
-  return;
-};
+  return
+}
 
-export default LocateControl;
+export default LocateControl

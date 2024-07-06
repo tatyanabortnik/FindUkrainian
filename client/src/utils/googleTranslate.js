2f22a3cbcfb9b16231e9e5c975455a7e47965c65
Tetiana Bortnyk <tatyanabortnik@gmail.com>
2024-07-06 10:24:44 +0200

    refactor: remove comments and files

--- client/src/utils/googleTranslate.js
+++
@@ -1,3 +0,0 @@
-//TODO add possibility to restore original language:
-
-//https://stackoverflow.com/questions/16281414/google-translator-how-to-manually-restore-original-language-with-javascript
--- server/models/seed.js
+++ server/models/seed.js
@@ -1,39 +1,38 @@
-// seed.js
-const { connectionString } = require('./index.js');
+const { connectionString } = require('./index.js')
 
-const mongoose = require('mongoose');
-const Business = require('./business');
-const Categories = require('./categories');
-const seedData = require('./mockBusiness');
-const seedCategories = require('./mockCategories.js');
+const mongoose = require('mongoose')
+const Business = require('./business')
+const Categories = require('./categories')
+const seedData = require('./mockBusiness')
+const seedCategories = require('./mockCategories.js')
 
 const seedDB = async () => {
-  const dbName = 'find_ukrainian';
+  const dbName = 'find_ukrainian'
 
   try {
     // Connect to MongoDB
-    await mongoose.connect(`${connectionString}/${dbName}`);
+    await mongoose.connect(`${connectionString}/${dbName}`)
 
     // Clear the database
-    await Business.deleteMany({});
-    console.log('Business collection cleared');
-    await Categories.deleteMany({});
-    console.log('Categories collection cleared');
+    await Business.deleteMany({})
+    console.log('Business collection cleared')
+    await Categories.deleteMany({})
+    console.log('Categories collection cleared')
 
     // Insert the seed data
-    await Business.insertMany(seedData);
-    console.log('Business collection seeded');
-    console.log(seedData);
-    await Categories.insertMany(seedCategories);
-    console.log('Categories collection seeded');
-    console.log(seedCategories);
+    await Business.insertMany(seedData)
+    console.log('Business collection seeded')
+    console.log(seedData)
+    await Categories.insertMany(seedCategories)
+    console.log('Categories collection seeded')
+    console.log(seedCategories)
 
     // Close the connection
-    mongoose.connection.close();
-    console.log('Connection closed');
+    mongoose.connection.close()
+    console.log('Connection closed')
   } catch (err) {
-    console.error(err);
+    console.error(err)
   }
-};
+}
 
-seedDB();
+seedDB()

1c2bdfbfe5c7ac61651d48b4592f8cb5eca3a405
Tetiana Bortnyk <tatyanabortnik@gmail.com>
2024-07-23 17:42:55 +0200

    feat: remove vercel.json; add environment check to use cloud database connection string in prod

--- server/models/index.js
+++ server/models/index.js
@@ -1,14 +1,20 @@
-const mongoose = require('mongoose');
-const dbName = 'find_ukrainian';
-const connectionString = 'mongodb://127.0.0.1:27017';
+require('dotenv').config()
+console.log(process.env)
+
+const mongoose = require('mongoose')
+const dbName = 'find_ukrainian'
+const connectionString =
+  process.env.NODE_ENV === 'prod'
+    ? process.env.MONGODB_URI
+    : 'mongodb://127.0.0.1:27017'
 
 async function connectDB() {
   try {
-    await mongoose.connect(`${connectionString}/${dbName}`);
-    console.log('Connected to MongoDB');
+    await mongoose.connect(`${connectionString}/${dbName}`)
+    console.log('Connected to MongoDB')
   } catch (error) {
-    console.log(`Error connecting to MongoDB', ${error}`);
+    console.log(`Error connecting to MongoDB', ${error}`)
   }
 }
 
-module.exports = { connectDB, connectionString };
+module.exports = { connectDB, connectionString }

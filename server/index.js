b1699434284f41fcd3762a9f068ee11543b44cbd
Tetiana Bortnyk <tatyanabortnik@gmail.com>
2024-07-25 23:23:18 +0200

    feat: add cors origin and env var

--- server/index.js
+++ server/index.js
@@ -1,22 +1,26 @@
-const express = require('express')
-const cors = require('cors')
-const router = require('./router.js')
-const { connectDB } = require('./models')
+const express = require('express');
+const cors = require('cors');
+const router = require('./router.js');
+const { connectDB } = require('./models');
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(express.json())
-app.use(router)
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(router);
 
 app.use((_req, res) => {
-  res.status(404).send('This route does not exist')
-})
+  res.status(404).send('This route does not exist');
+});
+(async () => {
+  await connectDB();
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+})();
 
-;(async () => {
-  await connectDB()
-  app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
-})()
-
-module.exports = app
+module.exports = app;

f86ce7a6468b4edc6288571dc96c016f3fc432f8
Tetiana Bortnyk <tatyanabortnik@gmail.com>
2024-07-25 11:57:00 +0200

    fix: upd baseUrl for prod

--- client/src/services/businessService.js
+++ client/src/services/businessService.js
@@ -1,29 +1,32 @@
-const baseUrl = 'http://localhost:3000';
+const baseUrl =
+  process.env.NODE_ENV === 'prod'
+    ? 'https://find-ukrainian.vercel.app'
+    : 'http://localhost:3000'
 
 export const getBusinesses = () => {
   return fetch(baseUrl + '/businesses')
     .then((res) => {
       if (!res.ok) {
-        throw new Error('Failed to fetch businesses');
+        throw new Error('Failed to fetch businesses')
       }
-      return res.json();
+      return res.json()
     })
     .catch((error) => {
-      console.error(`Failed to fetch businesses: ${error}`);
-      throw error;
-    });
-};
+      console.error(`Failed to fetch businesses: ${error}`)
+      throw error
+    })
+}
 
 export const getCategories = () => {
   return fetch(baseUrl + '/categories')
     .then((res) => {
       if (!res.ok) {
-        throw new Error('Failed to fetch categories');
+        throw new Error('Failed to fetch categories')
       }
-      return res.json();
+      return res.json()
     })
     .catch((error) => {
-      console.error(`Failed to fetch categories: ${error}`);
-      throw error;
-    });
-};
+      console.error(`Failed to fetch categories: ${error}`)
+      throw error
+    })
+}
